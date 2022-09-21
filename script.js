ea8682ceb2dafdbc1081a270d20c75cae69a570e
Diego Neves <pdcassiano@gmail.com>
2022-09-21 10:18:32 -0300

    criado efeitos de transição

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const label = document.getElementById('label')
+const on = document.getElementById('on')
+const container = document.querySelector('div.container')
+
+container.addEventListener('click', () => {
+  container.classList.toggle('off')
+  if (on.checked) {
+    on.checked = false
+  } else {
+    on.checked = true
+  }
+})
